0553327656cfc958bb7f1a54ee1acdb9e95a48e8
eyeballceo <samp50@eyeball.gg>
2019-07-11 00:16:06 -0400

    .

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            ad.eyeball.gg
           </a>
         </header>
       </div>
