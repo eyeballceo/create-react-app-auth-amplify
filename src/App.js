8b3b1cfbda8a44e793229c6399a28d1ea698c778
eyeballceo <samp50@eyeball.gg>
2019-07-11 15:52:04 -0400

    added popup for mobile users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component, Text } from 'react';
 import logo from './logo.svg';
-import work_on_mobile from './please_work_on_mobile.png';
 import './App.css';
-import { withAuthenticator, PhotoPicker } from 'aws-amplify-react'
+import { withAuthenticator, PhotoPicker } from 'aws-amplify-react';
 import Amplify, { Auth } from 'aws-amplify';
 import { SignIn } from "aws-amplify-react";
+import Popup from 'react-popup';
 import aws_exports from './aws-exports';
 import {isMobile} from 'react-device-detect';
 Amplify.configure(aws_exports);
 
 class App extends SignIn {
   render() {
-    if (isMobile==true) {
+    if (isMobile===false) {
       return (
         <div className="App">
           <header className="App-header">
@@ -20,9 +20,7 @@ class App extends SignIn {
         </div>
       );
     } else {
-      return (
-        <img src={work_on_mobile} alt="work_on_mobile"/>
-      );
+      Popup.alert('Please use eyeball ads on a desktop computer');
     }
   }
 }
