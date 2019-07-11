3ff9405b4cc3e28c76fd343866e1e590bd715b34
eyeballceo <samp50@eyeball.gg>
2019-07-11 09:41:59 -0400

    added mobile detection, renders diff for each

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Text } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, PhotoPicker } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import { SignIn } from "aws-amplify-react";
 import aws_exports from './aws-exports';
+import {isMobile} from 'react-device-detect';
 Amplify.configure(aws_exports);
 
 class App extends SignIn {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <PhotoPicker preview onLoad={dataURL => console.log(dataURL)} />
-        </header>
-      </div>
-    );
+    if (isMobile==false) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <PhotoPicker preview onLoad={dataURL => console.log(dataURL)} />
+          </header>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Text>eyeball ads is not available on mobile.</Text>
+          <Text>Please work on eyeball ads from a desktop computer.</Text>
+        </div>
+      );
+    }
   }
 }
 
