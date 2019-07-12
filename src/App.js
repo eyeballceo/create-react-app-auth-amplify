d94d771c97bb6b39f70795b000c8008d508f6a94
eyeballceo <samp50@eyeball.gg>
2019-07-11 21:13:56 -0400

    added eyeball ads logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component, Text } from 'react';
 import logo from './logo.svg';
+import eyeball_ads_logo from './eyeball_ads_logo.png';
 import './App.css';
 import { withAuthenticator, PhotoPicker } from 'aws-amplify-react';
 import Amplify, { Auth } from 'aws-amplify';
 import { SignIn } from "aws-amplify-react";
-import Popup from 'react-popup';
 import aws_exports from './aws-exports';
 import {isMobile} from 'react-device-detect';
+import 'react-s-alert/dist/s-alert-default.css';
 Amplify.configure(aws_exports);
 
 class App extends SignIn {
@@ -15,12 +16,13 @@ class App extends SignIn {
       return (
         <div className="App">
           <header className="App-header">
+            <img src={eyeball_ads_logo} height="25%" width="25%" alt="eyeball_ads_logo" />
             <PhotoPicker preview onLoad={dataURL => console.log(dataURL)} />
           </header>
         </div>
       );
     } else {
-      Popup.alert('Please use eyeball ads on a desktop computer');
+      
     }
   }
 }
